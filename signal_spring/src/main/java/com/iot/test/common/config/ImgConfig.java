59f5d807056df4b13ef82d4fa45c0455727cffaa
DJA <DJA@DJA-PC>
2018-04-20 12:17:23 +0900

    rrr

--- signal_spring/src/main/java/com/iot/test/common/config/ImgConfig.java
+++ signal_spring/src/main/java/com/iot/test/common/config/ImgConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 public class ImgConfig extends WebMvcConfigurerAdapter{
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 	    registry
